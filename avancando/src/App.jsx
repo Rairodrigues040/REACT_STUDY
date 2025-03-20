21c19a84e06a4cef261c7edb9ec28a8e1a40630f
Rai <rairodrigues@alu.ufc.br>
2025-03-20 20:01:44 -0300

    Aprendendo mais sobre react

--- avancando/src/App.jsx
+++ avancando/src/App.jsx
@@ -18,8 +18,40 @@ import Props from "./components/Props"
 //Desestruturando props
 import Desestruturacao from "./components/Desestruturacao"
 
+//renderização de listas com components
+const cars = [
+  { id: 1, brand: "Ferrari", km: 31 },
+  { id: 2, brand: "KIA", km: 20 },
+  { id: 3, brand: "Renault", km: 19 },
+]
+
+//React Fragments
+import Fragments from "./components/Fragments"
+
+// Children prop
+import Container from "./components/Container"
+
+// função em prop
+import ExecuteFunction from "./components/ExecuteFunction"
+
+//state lift
+import Message from "./components/Message"
+import { useState } from "react"
+import ChangeMessage from "./components/ChangeMessage"
+
 function App() {
 
+  // função em prop
+  function showMessage() {
+    console.log("Evento do componente pai")
+  }
+
+  //State lift
+  const [message, setmessage] = useState("")
+
+  const handleMessage = (msg) => {
+    setmessage(msg)
+  }
 
   return (
     <div style={{ paddingBottom: "500px" }}>
@@ -38,6 +70,29 @@ function App() {
       <Props name="Raí" />
       {/* Desestruturando props*/}
       <Desestruturacao brand="VW" km={999} color="Blue" />
+      {/* Reutilização de Components */}
+      <Desestruturacao brand="Fiat" km={123444} color="Vermelho" />
+      <Desestruturacao km={9987} color="azul" brand="Audi" />
+      {/* Renderização de listas com componentes */}
+      {cars.map((car) => (
+        <Desestruturacao
+          key={car.id}
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+        />
+      ))}
+      {/*React Fragments*/}
+      <Fragments />
+      {/*Children pop*/}
+      <Container>
+        <p>Alguma coisa</p>
+      </Container>
+      {/* função em prop */}
+      <ExecuteFunction myFunction={showMessage} />
+      {/* state lift */}
+      <Message msg={message} /> {/* esse exibe*/}
+      <ChangeMessage handleMessage={handleMessage} /> {/* esse altera*/}
     </div>
   )
 }
